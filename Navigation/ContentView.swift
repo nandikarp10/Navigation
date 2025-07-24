af591618fe5797aaea2a4e036c3927dc7a719664
Nandika Peddapuram <nandikarp10@gmail.com>
2025-07-24 12:26:58 -0500

    Added second page

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,11 +12,15 @@ struct ContentView: View {
         NavigationStack {
             VStack {
                 Text("This is the root view")
-                NavigationLink(destination: Text ("You've arrived to the Second View ")) {
+                NavigationLink(destination: SecondView()) {
                     Text ("Click Me!")
                 }
             }
+            .navigationTitle("Home")
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationBarHidden(true)
         }
+        
     }
 }
 
